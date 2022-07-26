558531937be63ddeb80222178b1eddea48e589ed
WooSangHwang <hws120@daum.net>
2022-07-26 21:57:13 +0900

    문자찾기 알고리즘

--- src/문자찾기/Main.java
+++ src/문자찾기/Main.java
@@ -1,5 +1,43 @@
 package 문자찾기;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+//        for(int i = 0; i < str.length(); i++) {
+//
+//            if (str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
+
+        for (char c : str.toCharArray()) {
+            if(c == t ) {
+                answer++;
+            }
+        }
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+
+
+        Scanner s = new Scanner(System.in);
+        String str = s.next();
+        char c = s.next().charAt(0);
+        System.out.println(T.solution(str, c));
+
+
+    }
 }
