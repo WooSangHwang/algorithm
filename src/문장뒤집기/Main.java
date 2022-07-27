aad3857ac5bd0ab2c7436739ed5712d3a25d1eaf
WooSangHwang <hws120@daum.net>
2022-07-27 19:26:47 +0900

    문장뒤집기 방법추가

--- src/문장뒤집기/Main.java
+++ src/문장뒤집기/Main.java
@@ -5,11 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public void solution(String[] arr) {
-
+//        StringBuffer의 revesrs()를 이용한 방법
+//        for (String str : arr) {
+//            StringBuffer b = new StringBuffer(str);
+//            System.out.println(b.reverse().toString());
+//        }
+//        직접하기 - 1
+//        for (String str : arr ) {
+//            char[] cArr = str.toCharArray();
+//            for (int i = 0; i < str.length()/2; i++) {
+//                char tmp = str.charAt(i);
+//                cArr[i] = cArr[(str.length()-1)-i];
+//                cArr[(str.length()-1)-i] = tmp;
+//
+//            }
+//            System.out.println(cArr);
+//        }
+//        직접하기 - 2
         for (String str : arr) {
-            StringBuffer b = new StringBuffer(str);
-            System.out.println(b.reverse().toString());
-        }
+            char[] cArr = str.toCharArray();
+            int lt = 0, rt = str.length() - 1;
+            while (lt < rt) {
+                char tmp = cArr[lt];
+                cArr[lt] = cArr[rt];
+                cArr[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            System.out.println(cArr);
+         }
+
     }
 
 
