83c975c07ce47db0965be1786d1369e4f2490b28
WooSangHwang <hws120@daum.net>
2022-07-29 00:53:09 +0900

    특정문자뒤집기 수정

--- src/특정문자뒤집기/Main.java
+++ src/특정문자뒤집기/Main.java
@@ -1,5 +1,6 @@
 package 특정문자뒤집기;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main {
@@ -11,19 +12,31 @@ public class Main {
 
 
         while (lt < rt) {
-            if((65 <= str.charAt(lt) && str.charAt(lt) <= 90) || (97 <= str.charAt(lt) && str.charAt(lt) <= 122)){
-
-                if ((65 <= str.charAt(rt) && str.charAt(rt) <= 90) || (97 <= str.charAt(rt) && str.charAt(rt) <= 122)) {
-                    char tmp = cArr[lt];
-                    cArr[lt] = cArr[rt];
-                    cArr[rt] = tmp;
-                    lt++;
-                    rt--;
-                } else {
-                    rt--;
-                }
-            } else {
+//            직접 만든것.
+//            if((65 <= str.charAt(lt) && str.charAt(lt) <= 90) || (97 <= str.charAt(lt) && str.charAt(lt) <= 122)){
+//                if((65 <= str.charAt(rt) && str.charAt(rt) <= 90) || (97 <= str.charAt(rt) && str.charAt(rt) <= 122)) {
+//                    char tmp = cArr[lt];
+//                    cArr[lt] = cArr[rt];
+//                    cArr[rt] = tmp;
+//                    lt++;
+//                    rt--;
+//                } else {
+//                    rt--;
+//                }
+//            } else {
+//                lt++;
+//            }
+//            인프런 방법
+            if (!Character.isAlphabetic(cArr[lt])) {
                 lt++;
+            } else if (!Character.isAlphabetic(cArr[rt])) {
+                rt--;
+            } else {
+                char tmp = cArr[lt];
+                cArr[lt] = cArr[rt];
+                cArr[rt] = tmp;
+                lt++;
+                rt--;
             }
         }
 
