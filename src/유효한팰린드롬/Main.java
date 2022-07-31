956489ec97bd2ea471af3c3617285aafb4a5fb77
WooSangHwang <hws120@daum.net>
2022-07-31 21:48:51 +0900

    유효한팰린드롬

---
+++ src/유효한팰린드롬/Main.java
@@ -0,0 +1,38 @@
+package 유효한팰린드롬;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public String solution(String str) {
+
+        String anwser = "YES";
+        str = str.toLowerCase().replaceAll("[^a-z]","");
+
+        for (int j = 0; j < str.length() / 2; j++) {
+//                System.out.println(strArr[i].charAt(j));
+//                System.out.println(strArr[strArr.length - 1 - i].charAt(strArr[i].length() -1 - j));
+            if (str.charAt(j) != str.charAt(str.length() - 1 - j)) {
+                anwser = "NO";
+            }
+        }
+//        String reverseStr = new StringBuilder(str).reverse().toString();
+//        if (!str.equals(reverseStr)) {
+//            anwser = "NO";
+//        }
+
+        //StringBuiler(str).rev
+
+        return anwser;
+    }
+
+    public static void main(String[] args) {
+        Main t = new Main();
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.println(t.solution(str));
+
+    }
+
+}
